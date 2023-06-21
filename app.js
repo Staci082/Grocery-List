aa181943a83dd9e1b90c73bf55c40dc0d43221b3
Staci082 <sjjohnson082@gmail.com>
2023-06-21 15:12:39 +0200

    buttons working

--- app.js
+++ app.js
@@ -12,20 +12,28 @@ class GroceryList {
     }
 
     createItem() {
-        itemsArray.push(input.value)
-        localStorage.setItem('items', JSON.stringify(itemsArray))
-        location.reload()
+        itemsArray.push(input.value);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        this.displayItems();
     }
 
-    
- displayItems() {
-    let items = ""
-    for(let i = 0; i < itemsArray.length; i++) {
-        items += ` <li>${itemsArray[i]}</li>`
-}
+    deleteItem(i) {
+        itemsArray.splice(i, 1);
+        localStorage.setItem("items", JSON.stringify(itemsArray));
+        this.displayItems();
+    }
 
-list.innerHTML = items
- }
+    displayItems() {
+        let items = "";
+        for (let i = 0; i < itemsArray.length; i++) {
+            items += `<li class="item">
+                <p>${itemsArray[i]}</p>
+                <i class="fa-regular fa-square-check deleteBtn"></i>
+            </li>`;
+        }
+        list.innerHTML = items;
+    }
+
 }
 
 let app = new GroceryList();
@@ -33,8 +41,30 @@ let app = new GroceryList();
 addButton.addEventListener('click', (e) => {
     e.preventDefault()
     app.createItem(input.value)
+    input.value = ""
+    location.reload()
 }) 
+
+
+list.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.classList.contains('deleteBtn')) {
+        const listItem = e.target.closest('li');
+        const itemIndex = Array.from(list.children).indexOf(listItem);
+        listItem.remove();
+        app.deleteItem(itemIndex);
+        location.reload()
+    }
+});
 
 window.onload = function() {
     app.displayItems()
     }
+
+input.addEventListener("keypress", function(e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            addButton.click();
+        }
+});
